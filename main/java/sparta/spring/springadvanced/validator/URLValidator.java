f9b78c41a407d8137703bacd14dd4f688275904c
SMJin <sstaw@naver.com>
2022-04-02 02:23:45 +0900

    week03-05 Edge case

---
+++ main/java/sparta/spring/springadvanced/validator/URLValidator.java
@@ -0,0 +1,21 @@
+package sparta.spring.springadvanced.validator;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class URLValidator {
+    public static boolean isValidUrl(String url)
+    {
+        try {
+            new URL(url).toURI();
+            return true;
+        }
+        catch (URISyntaxException exception) {
+            return false;
+        }
+        catch (MalformedURLException exception) {
+            return false;
+        }
+    }
+}
